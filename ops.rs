fc7d3327bd9f4309a22b7f73477644475b0f49c9
NazerkeBS <seinaz1997@gmail.com>
2018-08-13 19:26:23 +0200

    ops in generics

--- ops.rs
+++ ops.rs
@@ -8,22 +8,34 @@ use timely::dataflow::operators::{Inspect, ToStream, Delay};
 use timely::dataflow::channels::pact::Pipeline;
 use timely::dataflow::operators::Operator;
 use timely::progress::timestamp::RootTimestamp;
+use std::ops::Add;
+use std::ops::Mul;
+use std::cmp::Ord;
+use std::ops::Div;
 
-trait Aggregate {
-	fn max(&self) -> Self;
-	fn min(&self) -> Self;
-	fn product(&self) -> Self;
+trait Aggregate<S: Scope, T> where T: Ord + Clone + Copy{
+	fn max(&self) -> Stream<S, T>;
+	fn min(&self) -> Stream<S, T>;	
 }
 
-trait Sum{
-	fn sum(&self) -> Self;
+trait TakeWhile <D: Data>{
+	fn take_while <L: Fn(&D) -> bool + 'static> (&self, predicate: L) -> Self;
 }
+
+trait Sum<S: Scope, T> where T: Add<Output=T> + Copy + Clone + Default {
+	fn sum(&self) -> Stream<S,T>;
+}
+/*
+trait Product<S: Scope, T> where T: Mul<Output=T> + Copy + Clone + Default {
+	fn product(&self) -> Stream<S,T>;
+}
+*/
 
 trait Mode<S: Scope> {
 	fn mode(&self) -> Stream <S,(u64, u64)>;
 }
 
-trait Average <S: Scope>{
+trait Average <S: Scope, T> where T: Add<Output=T> + Copy + Clone + Default  + Div<Output=f64>{
   	fn avg (&self) -> Stream<S, f64>;
 }
 
@@ -54,17 +66,18 @@ impl <S: Scope> Mode<S> for Stream<S, u64>{
 	}	
 }
 
-impl <S : Scope> Sum for Stream<S, u64>{
-	fn sum(&self) -> Stream<S, u64> {
+impl <S: Scope, T: Data> Sum<S, T> for Stream<S, T> where T: Add<Output=T> + Copy + Clone + Default{
+    fn sum(&self) -> Stream<S,T>{
         let mut stash = HashMap::new();
         self.unary_notify(Pipeline, "Sum", vec![], move |input, output, notificator|{
             input.for_each(|time, data|{
-                let sum = stash.entry(time.time().clone()).or_insert(0);
+                let sum = stash.entry(time.time().clone()).or_insert(Default::default());
                     for datum in data.iter() {
-                        *sum += datum;
+                        *sum = *sum + *datum;
                     }
-                    notificator.notify_at(time.delayed(&time));
+                notificator.notify_at(time.delayed(&time));        
             });
+
             notificator.for_each(|time,_,_| {
                 if let Some(sum) = stash.remove(&time) {
                     output.session(&time).give(sum);
@@ -73,17 +86,48 @@ impl <S : Scope> Sum for Stream<S, u64>{
         })
     }
 }
+/*
+impl <S: Scope, T: Data> Product<S, T> for Stream<S, T> where T: Mul<Output=T> + Copy + Clone + Default{
+	fn product(&self) -> Stream<S,T>{
+		self.unary(Pipeline, "Product", |_cap, _info| move |input, output|{
+			input.for_each(|time,data|{
+				let mut prod = 1;
+				for datum in data.iter(){
+					prod = prod * (*datum);	
+				}
+				output.session(&time).give(prod);
+			});
+		})
+	}
+}
+*/
 
+impl <S: Scope, D: Data> TakeWhile<D> for Stream<S, D>{
+	fn take_while<L: Fn(&D) -> bool + 'static> (&self, predicate: L) -> Stream<S, D>{
+		self.unary(Pipeline, "TakeWhile", |_cap, _info| move|input, output|{
+			while let Some((time, data)) = input.next(){
+			    for datum in data.drain(..){ // extract values
+					if predicate(&datum){
+						output.session(&time).give(datum);
+					}else {
+						break;
+					}
+				}	
+			}
 
-impl <S: Scope> Aggregate for Stream<S, u64>{
-	fn max(&self) -> Stream<S,u64> {
+		})
+	}
+}
+
+impl <S: Scope, T: Data> Aggregate<S,T> for Stream<S, T> where T: Ord + Clone + Copy{
+	fn max(&self) -> Stream<S,T> {
 		self.unary(Pipeline, "Maximum", |_cap, _info| move |input, output|{
 				while let Some((time, data)) = input.next(){
 					let mut session = output.session(&time);
 					let mut largest = data[0];
-					for d in data.iter(){
-						if d > &largest {
-							largest = *d;
+					for d in data.drain(..){
+						if d > largest {
+							largest = d;
 						} 
 					}
 					session.give(largest);
@@ -91,14 +135,14 @@ impl <S: Scope> Aggregate for Stream<S, u64>{
 		})
 	}
 
-	fn min(&self) -> Stream<S,u64>{
+	fn min(&self) -> Stream<S,T>{
 		self.unary(Pipeline, "Minimum", |_cap, _info| move |input, output|{
 				while let Some((time, data)) = input.next(){
 					let mut session = output.session(&time);
 					let mut smallest = data[0];
-					for d in data.iter(){
-						if &smallest > d {
-							smallest = *d;
+					for d in data.drain(..){
+						if smallest > d {
+							smallest = d;
 						}
 					}
 					session.give(smallest);
@@ -107,32 +151,19 @@ impl <S: Scope> Aggregate for Stream<S, u64>{
 		})
 	}
 
-	fn product(&self) -> Stream <S, u64>{
-		self.unary(Pipeline, "Product", |_cap, _info| move |input, output|{
-			input.for_each(|time,data|{
-				let mut prod = 1;
-				for datum in data.iter(){
-					prod *= datum;	
-				}
-				output.session(&time).give(prod);
-			});
-		})
-	}	
-
 }
 
-
-impl <S: Scope> Average <S> for Stream <S, u64>{
+impl <S: Scope, T: Data> Average <S, T> for Stream <S, T> where T: Add<Output = T> + Copy + Clone + Default + Div<Output=f64>{
 	fn avg (&self) -> Stream<S, f64> {
 		self.unary(Pipeline, "Average", |_cap, _info| move |input, output|{
 			input.for_each(|time, data|{
-				let mut sum = 0;
-				let mut cnt = 0;
-				for datum in data.iter(){
-					sum += datum;
-					cnt += 1;
+				let mut sum : T = Default::default();
+				let mut cnt: f64 = 0.0;
+				for datum in data.drain(..){
+					sum = sum + datum;
+					cnt += 1.0;
 				}
-				let avg = (sum as f64) / (cnt as f64);		
+				let avg : f64 = f64::from(sum) / cnt;		
 				output.session(&time).give(avg);
 			});		 
 		
@@ -145,7 +176,7 @@ fn main(){
 	//sum up the stream of integers
 	timely::example(|scope|{
 		(0u64..100).to_stream(scope)
-		.delay(|x,t| RootTimestamp::new(*x /10))
+		.delay(|x,_t| RootTimestamp::new(*x /10))
 		.sum()
 		.inspect_batch(|t,x| println!("time ={:?} sum ={:?}", t, x));
 	});
@@ -182,8 +213,15 @@ fn main(){
  	//product
  	timely::example(|scope|{
 		(1..10).to_stream(scope)
-			   .product()
+			   //.product()
 			   .inspect(|x| println!("{:?}", x));
+	});
+
+	//take until predicate holds
+	timely::example(|scope|{
+		(1..10).to_stream(scope)
+			.take_while(|x| x < &4)
+			.inspect(|x| println!("{:?}", x));
 	});
 
 }
