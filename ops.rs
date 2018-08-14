688ea51185b3c51ff2fa3364a5c855e645a81786
NazerkeBS <seinaz1997@gmail.com>
2018-08-14 16:19:45 +0200

    generic operators

--- ops.rs
+++ ops.rs
@@ -3,15 +3,16 @@ extern crate timely;
 use std::collections::HashMap;
 
 use timely::Data;
-use timely::dataflow::{Stream,Scope};
-use timely::dataflow::operators::{Inspect, ToStream, Delay};
+use timely::dataflow::{Stream,Scope, InputHandle};
+use timely::dataflow::operators::{Inspect, ToStream, Delay, Input, Probe};
 use timely::dataflow::channels::pact::Pipeline;
 use timely::dataflow::operators::Operator;
 use timely::progress::timestamp::RootTimestamp;
 use std::ops::Add;
 use std::ops::Mul;
 use std::cmp::Ord;
-use std::ops::Div;
+use std::hash::Hash;
+use std::cmp::Eq;
 
 trait Aggregate<S: Scope, T> where T: Ord + Clone + Copy{
 	fn max(&self) -> Stream<S, T>;
@@ -25,43 +26,56 @@ trait TakeWhile <D: Data>{
 trait Sum<S: Scope, T> where T: Add<Output=T> + Copy + Clone + Default {
 	fn sum(&self) -> Stream<S,T>;
 }
-/*
-trait Product<S: Scope, T> where T: Mul<Output=T> + Copy + Clone + Default {
+
+trait Product<S: Scope, T> where T: Mul<Output=T> + Copy + Clone  {
 	fn product(&self) -> Stream<S,T>;
 }
-*/
 
-trait Mode<S: Scope> {
-	fn mode(&self) -> Stream <S,(u64, u64)>;
+
+trait Mode<S: Scope, T: Data> where T: Eq + Hash{
+	fn mode(&self) -> Stream <S,(T, u64)>;
 }
 
-trait Average <S: Scope, T> where T: Add<Output=T> + Copy + Clone + Default  + Div<Output=f64>{
+trait Average <S: Scope, T> where T: Into<f64> {
   	fn avg (&self) -> Stream<S, f64>;
 }
 
-impl <S: Scope> Mode<S> for Stream<S, u64>{
-	fn mode(&self) -> Stream <S,(u64, u64)>{
-		self.unary(Pipeline, "Mode", |_cap, _info| move |input, output|{		
+impl <S: Scope, T: Data> Mode<S, T> for Stream<S, T> where T: Eq + Hash{
+	fn mode(&self) -> Stream <S,(T, u64)>{
+		self.unary(Pipeline, "Mode", |_cap, _info| {
+
+			let mut map = HashMap::new();
+
+			move |input, output|{		
 				while let Some((time,data)) = input.next(){
 					let mut session = output.session(&time);
-					let mut map = HashMap::new();
 					for datum in data.iter(){
-						let count = map.entry(datum).or_insert(0);
+						let count = map.entry(datum.clone()).or_insert(0);
 						*count += 1;
 					}
                     
-                    let mut max_cnt = 0;
-                    let mut value = 0; 
-					for (c, v) in map {
-						if &max_cnt < c {
-							max_cnt = *c;
-							value = v;
+                    let mut iterator = map.iter();
+                    if let Some((mut max_key, mut max_cnt)) = iterator.next() {
+
+						for (v, c) in iterator {
+							if max_cnt < c {
+								max_cnt = c;
+								max_key = v;
+							}
 						}
-					}
-					let tuple = (value, max_cnt);
-					session.give(tuple);
+
+						let tuple = (max_key.clone(), max_cnt.clone());
+						session.give(tuple);
+                    }
+
+                    /*
+                    if let Some((key,cnt)) = map.into_iter().max_by_key(|pair| pair.1) {
+						session.give((key,cnt));                    	
+                    }
+                    */
+
 				}
-			
+			}
 		})
 	}	
 }
@@ -86,21 +100,28 @@ impl <S: Scope, T: Data> Sum<S, T> for Stream<S, T> where T: Add<Output=T> + Cop
         })
     }
 }
-/*
-impl <S: Scope, T: Data> Product<S, T> for Stream<S, T> where T: Mul<Output=T> + Copy + Clone + Default{
+
+impl <S: Scope, T: Data> Product<S, T> for Stream<S, T> where T: Mul<Output=T> + Copy + Clone {
 	fn product(&self) -> Stream<S,T>{
 		self.unary(Pipeline, "Product", |_cap, _info| move |input, output|{
 			input.for_each(|time,data|{
-				let mut prod = 1;
+				let mut prod : Option<T> = None;
 				for datum in data.iter(){
-					prod = prod * (*datum);	
+					if let Some(x) = prod {
+						prod = Some(x * (*datum));
+					}
+					else {
+						prod = Some(*datum);
+					} 	
 				}
-				output.session(&time).give(prod);
+				if let Some(x) = prod {
+					output.session(&time).give(x);
+				}
 			});
 		})
 	}
 }
-*/
+
 
 impl <S: Scope, D: Data> TakeWhile<D> for Stream<S, D>{
 	fn take_while<L: Fn(&D) -> bool + 'static> (&self, predicate: L) -> Stream<S, D>{
@@ -153,17 +174,17 @@ impl <S: Scope, T: Data> Aggregate<S,T> for Stream<S, T> where T: Ord + Clone + 
 
 }
 
-impl <S: Scope, T: Data> Average <S, T> for Stream <S, T> where T: Add<Output = T> + Copy + Clone + Default + Div<Output=f64>{
+impl <S: Scope, T: Data> Average <S, T> for Stream <S, T> where T: Into<f64>{
 	fn avg (&self) -> Stream<S, f64> {
 		self.unary(Pipeline, "Average", |_cap, _info| move |input, output|{
 			input.for_each(|time, data|{
-				let mut sum : T = Default::default();
+				let mut sum : f64 = Default::default();
 				let mut cnt: f64 = 0.0;
 				for datum in data.drain(..){
-					sum = sum + datum;
+					sum = sum + datum.into();
 					cnt += 1.0;
 				}
-				let avg : f64 = f64::from(sum) / cnt;		
+				let avg : f64 = sum / cnt;		
 				output.session(&time).give(avg);
 			});		 
 		
@@ -197,7 +218,8 @@ fn main(){
    
    	// mode of a vector 
  	timely::example(|scope|{
-   		let v = vec![1,2,2,2,3,4,4];
+   		//let v = vec![1,2,2,2,3,4,4];
+   		let v = vec![0; 20000];
    		v.to_stream(scope)
    		.mode()
    		.inspect(|x| println!("{:?}", x ));
@@ -223,5 +245,40 @@ fn main(){
 			.take_while(|x| x < &4)
 			.inspect(|x| println!("{:?}", x));
 	});
+
+
+
+	// initializes and runs a timely dataflow.
+    timely::execute_from_args(std::env::args(), |worker| {
+
+        let index = worker.index();
+        let mut input = InputHandle::new();
+
+        // create a new input, exchange data, and inspect its output
+        let probe = worker.dataflow(|scope|
+            scope.input_from(&mut input)
+                 .mode()
+                 .inspect_batch(move |t,xs| println!("worker {}:\thello {:?} @ {:?}", index, xs, t))
+                 .probe()
+        );
+
+        let timer = std::time::Instant::now();
+
+        // introduce data and watch!
+        let v = vec![1,2,2,2,3,4,4];
+        for (_round, value) in v.into_iter().enumerate() {
+            if index == 0 {
+                input.send(value);
+            }
+
+            let elapsed = timer.elapsed();
+            let elapsed_ns = elapsed.as_secs() * 1000000000 + (elapsed.subsec_nanos() as u64);
+
+            input.advance_to(elapsed_ns);
+            while probe.less_than(input.time()) {
+                worker.step();
+            }
+        }
+    }).unwrap();
 
 }
